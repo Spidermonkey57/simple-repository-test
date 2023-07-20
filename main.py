5c8ad1153a50fd27b339e254ad804aff7af43a49
julsanchez <23642025-julsanchez@users.noreply.replit.com>
2023-07-20 21:35:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+print("welcome user!")
+company_name = input("what is your company's name?")
+feet = int(input("Enter the number feet of fiber optic to be installed"))
+if feet > 500:
+ rate = 0.50
+elif feet > 250:
+ rate = 0.70
+elif feet > 100:
+  rate = 0.80
+else:
+  rate = 0.87
+total_cost = feet * rate
+print("feet requested:", feet)
+print ("the total cost is:",total_cost)
+print("Thank you", company_name)
